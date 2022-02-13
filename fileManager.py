9ace09c24e6d911d79dd73f4d6909ab02b953fd3
Thoren <thoren.grube@gmail.com>
2022-02-13 16:55:51 +0100

    remove 'voltage' from measurement, add moisture labels to gui

--- fileManager.py
+++ fileManager.py
@@ -1,12 +1,32 @@
 import os.path
 import csv
 from datetime import datetime
+import json
+import jsons
 
 class fileManager():
     "Class which contains functions to save and load data from files"
 
     def __init__(self) -> None:
         pass
+
+
+    def read_json(self, path) -> str:
+        try:
+            with open(path, 'r') as handle:
+                return json.load(handle)
+        except FileNotFoundError:
+            print('Could not read JSON file. File not found.')
+            return ""
+        except json.JSONDecodeError:
+            print('Could not read JSON file. Could not been parsed as JSON.')
+            return ""
+
+
+    def dump_data_to_json(self, data, path) -> None:
+        with open(path, 'w') as outfile:
+            json_string = jsons.dump(data)
+            json.dump(json_string, outfile, indent=4, sort_keys=True)
 
 
     # add a 'n.a.' as measured data, if there are gaps in flowerpot slot-array
@@ -18,7 +38,7 @@ class fileManager():
 
         for slot in range(maximum_slot + 1):
             if slot == flowerpots[pot_iterator].slot:
-                data_array.append(flowerpots[pot_iterator].actual_moisture.value)
+                data_array.append(flowerpots[pot_iterator].actual_moisture)
                 pot_iterator += 1
             else:
                 data_array.append('n.a.')
@@ -35,7 +55,7 @@ class fileManager():
                 writer.writerow(header1)
             
 
-        data_to_write = [timestamp] + self.build_data_to_write(flowerpots)
+        data_to_write = [timestamp.strftime("%Y-%m-%d %H:%M:%S")] + self.build_data_to_write(flowerpots)
         with open(filename, 'a') as f:
             writer = csv.writer(f)
             writer.writerow(data_to_write)
--- flowerpotManager.py
+++ flowerpotManager.py
@@ -2,16 +2,10 @@ from pumpControl import pump
 import registerControl
 import adcControl
 import fileManager
+import exceptions
+
 from datetime import datetime
 import time
-
-class measurement:
-    "Contains a measurement with voltage and value"
-
-    def __init__(self, voltage: int, value: float) -> None:
-        self.value = value
-        self.voltage = voltage
-        return
 
 class flowerpot:
     "Class which represents a flowerpot."
@@ -20,13 +14,13 @@ class flowerpot:
         self.slot = slot
         self.name = name
         self.is_active = True
-        self.actual_moisture = measurement
+        self.actual_moisture = 0
         self.expected_moisture = 0
         return
 
 
-    def set_actual_moisture(self, moisture: measurement) -> None:
-        self.actual_moisture = moisture
+    def set_actual_moisture(self, actual_moisture) -> None:
+        self.actual_moisture = actual_moisture
         return
 
     def set_expected_moisture(self, moisture: int) -> None:
@@ -53,8 +47,8 @@ class flowerpotManager:
     "This class holds all flowerpots and provides handling functions."
 
     def __init__(self, config) -> None:
-        self.all_flowerpots = []
         self.output_path = config.config['PATHS']['DATA_OUTPUT_PATH']
+        self.pot_dump_path = config.config['PATHS']['POT_DUMP_PATH']
         self.saturation_sleep_time = int(config.config['PARAMS']['SAMPLE_SATURATION_SLEEP_IN_MS']) / 1000
         self.pump = pump(config.pump)
         self.sensor_register = registerControl.register(config.ser, config.sclk, config.rclk_sensor)
@@ -62,38 +56,121 @@ class flowerpotManager:
         self.adc = adcControl.adc()
         self.file_manager = fileManager.fileManager()
         self.last_timestamp = int
+
+        self.pot_collection = []
+        self.initialize_pot_collection(self.pot_dump_path)
         return
 
 
-    def add_flowerpot(self, slot: int, name: str) -> flowerpot:
+    def initialize_pot_collection(self, path) -> None:
+        print(f"Initialize after restart.")
+        parsed_pots = self.file_manager.read_json(path)
+
+        for parsed_pot in parsed_pots:
+            name = parsed_pot['name']
+            slot = parsed_pot['slot']
+            new_pot = flowerpot(slot, name)
+
+            new_pot.is_active = parsed_pot['is_active']
+            new_pot.expected_moisture = parsed_pot['expected_moisture']
+
+            self.add_pot(new_pot)
+
+        print(f"Initialization complete.")
+        return
+
+
+    def clear_pot_collection(self) -> None:
+        self.pot_collection.clear()
+        self.dump_pot_collection()
+        return
+
+
+    def dump_pot_collection(self) -> None:
+        self.file_manager.dump_data_to_json(self.pot_collection, self.pot_dump_path)
+        return
+
+
+    def add_pot(self, new_pot: flowerpot) -> None:
+        def fill_slot_gap_with_dummy_pots(start_index, end_index):
+            def add_dummy_pot(slot):
+                new_dummy_pot = flowerpot(slot, "<empty>")
+                self.pot_collection.append(new_dummy_pot)
+                print(f"Dummy pot added to collection at slot {slot}")
+                return
+
+            index = start_index
+            while index <= end_index:
+                add_dummy_pot(index)
+                index += 1
+            return
+
+        def update_pot(new_pot):
+            if self.pot_collection[new_pot.slot].name != "<empty>":
+                print(f"Slot {new_pot.slot} already occupied! Pot has not been added.")
+                raise exceptions.PotDoubletteError
+            
+            self.pot_collection[new_pot.slot] = new_pot
+            print(f"Dummy pot at slot {new_pot.slot} overwritten!")
+            print(f"Pot \"{new_pot.name}\" added to collection at slot {new_pot.slot}.")
+            return
+
+            
+        pot_collection_length = len(self.pot_collection)
+
+        if pot_collection_length == new_pot.slot:
+            self.pot_collection.append(new_pot)
+            print(f"Pot \"{new_pot.name}\" added to collection at slot {new_pot.slot}.")
+        elif pot_collection_length < new_pot.slot:
+            fill_slot_gap_with_dummy_pots(pot_collection_length, new_pot.slot - 1)
+            self.pot_collection.append(new_pot)
+            print(f"Pot \"{new_pot.name}\" added to collection at slot {new_pot.slot}.")
+        elif pot_collection_length > new_pot.slot:
+            update_pot(new_pot)
+
+        self.retrieve_single_data(new_pot)
         
-        for pot in self.all_flowerpots:
-            if slot == pot.slot:
-                print("Slot is already occupied! Pot has not been added.")
-                return None
-            elif name == pot.name:
-                print("Pot name exists already!")
-
-        new_pot = flowerpot(slot, name)
-        self.all_flowerpots.append(new_pot)
-        self.all_flowerpots.sort(key=lambda pot: pot.slot)
-        return new_pot
+        self.dump_pot_collection()
+        return
 
 
-    def remove_flowerpot(self, slot: int) -> None:
-        def find_pot_by_slot():
-            for pot in self.all_flowerpots:
-                if pot.slot == slot:
-                    return pot
+    def remove_pot(self, slot: int) -> None:
+        def remove_last_dummy_pots():
+            last_pot_is_dummy = True
+            while last_pot_is_dummy:
+                if self.pot_collection[-1].name == "<empty>":
+                    print(f"Dummy pot at slot {self.pot_collection[-1].slot} removed.")
+                    del self.pot_collection[-1]
+                else:
+                    last_pot_is_dummy = False
+            return
 
-        self.all_flowerpots.remove(find_pot_by_slot())
+        def overwrite_slot_with_dummy_pot(slot):
+                new_dummy_pot = flowerpot(slot, "<empty>")
+                self.pot_collection[slot] = new_dummy_pot
+                print(f"Pot at slot {slot} removed and replaced by dummy pot.")
+                return
+
+        pot_collection_length = len(self.pot_collection)
+
+        if pot_collection_length - 1 == slot:
+            print(f"Pot \"{self.pot_collection[slot].name}\" removed.")
+            del self.pot_collection[slot]
+            remove_last_dummy_pots()
+        elif pot_collection_length - 1 < slot:
+            print(f"No pot listed for slot {slot}")
+        elif pot_collection_length - 1 > slot:
+            overwrite_slot_with_dummy_pot(slot)
+        
+        self.dump_pot_collection()
+        return
 
 
     def retrieve_single_data(self, pot: flowerpot) -> None:
         self.sensor_register.set_single_bit(pot.slot)
         time.sleep(self.saturation_sleep_time) # sleep to wait for sensor saturation
         adc_channel_output = self.adc.retrieve_data()
-        pot.set_actual_moisture(measurement(adc_channel_output.voltage, adc_channel_output.value))
+        pot.set_actual_moisture(adc_channel_output.value)
 
         self.sensor_register.clear()
         return
@@ -101,6 +178,6 @@ class flowerpotManager:
 
     def retrieve_all_data(self) -> None:
         self.last_timestamp = datetime.now()
-        [self.retrieve_single_data(pot) for pot in self.all_flowerpots if pot.is_active]
-        self.file_manager.write_data_to_file(self.output_path, self.last_timestamp, self.all_flowerpots)
+        [self.retrieve_single_data(pot) for pot in self.pot_collection if pot.is_active and pot.name != "<empty>"]
+        self.file_manager.write_data_to_file(self.output_path, self.last_timestamp, self.pot_collection)
         return
